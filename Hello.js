67929d3ace9f273e1ebe2e2f4c8390fc7e5edefb
Shantanu Dubey <dubeyshantanu2@gmail.com>
2022-12-26 16:22:21 +0530

    Examples for components testing

--- Hello.js
+++ Hello.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 export default function Hello({ name }) {
   return (
     <View>
       <Text>Hello, {name}!</Text>
+      <Image
+        style={{ height: 100, width: 100 }}
+        source={require('./assets/favicon.png')}
+        accessibilityLabel="fav waving"
+      />
     </View>
   );
 }
--- Hello.spec.js
+++ Hello.spec.js
@@ -1,11 +1,25 @@
-import { render, screen } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import React from 'react';
 
-import Hello from './Hello';
+// import Hello from './Hello';
 
-describe('Hello', () => {
-  it('display the passed in name', () => {
-    render(<Hello name="Shantanu" />);
-    expect(screen.getByText('Hello, Shantanu!')).toBeVisible();
+import NewMessageForm from './NewMessageForm';
+// describe('Hello', () => {
+//   it('display the passed in name', () => {
+//     render(<Hello name="Shantanu" />);
+//     expect(screen.getByText('Hello, Shantanu!')).toBeVisible();
+//   });
+//   it('displays the squirrel image', () => {
+//     expect(screen.getByLabelText('fav waving')).toBeVisible();
+//   });
+// });
+
+describe('NewMessageForm', () => {
+  describe('clicking send', () => {
+    it('clear the message field', () => {
+      render(<NewMessageForm />);
+      fireEvent.changeText(screen.getByPlaceholderText('Message'), 'Hello world');
+      fireEvent.press(screen.getByText('Send'));
+    });
   });
 });
